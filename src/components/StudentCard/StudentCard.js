ca92a5044fac2a18e099e72fad7ed9f0d97765ec
benche <ben.che9@gmail.com>
2019-01-19 18:10:53 -0500

    adding toggle

--- src/components/StudentCard/StudentCard.js
+++ src/components/StudentCard/StudentCard.js
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './StudentCard.css';
 
-export default function StudentCard(props) {
-	return (
-		<div className="student-card">
-			<div className="student-picture-container">
-				<div
-					className="student-picture"
-					style={{ backgroundImage: `url(${props.pic})` }}
-				/>
-				<div className="student-description-container">
-					<p className="student-description-title">
-						{props.firstName} {props.lastName}
-					</p>
-					<p className="student-description-content">
-						Email: {props.email}
-					</p>
-					<p className="student-description-content">
-						Company: {props.company}
-					</p>
-					<p className="student-description-content">
-						Skill: {props.skill}
-					</p>
-					<p className="student-description-content">
-						Average: {calculateAverage(props.grades)}
-					</p>
+class StudentCard extends Component {
+	state = { toggled: false };
+
+	// CALCULATES AVERAGE OF ALL GRADES
+	calculateAverage(gradeArr) {
+		return (
+			gradeArr.reduce((a, b) => {
+				return Number(a) + Number(b);
+			}) / gradeArr.length
+		).toFixed(2);
+	}
+
+	// RENDERS TOGGLE BUTTON DEPENDING ON STATE
+	renderToggle() {
+		if (this.state.toggled) {
+			return '--';
+		}
+		return '+';
+	}
+
+	handleToggle() {
+		this.setState({
+			toggled: !this.state.toggled
+		});
+	}
+
+	render() {
+		return (
+			<div className="student-card">
+				<div className="student-picture-container">
+					<div
+						className="student-picture"
+						style={{ backgroundImage: `url(${this.props.pic})` }}
+					/>
+					<div className="student-description-container">
+						<p className="student-description-title">
+							{this.props.firstName} {this.props.lastName}
+						</p>
+						<p className="student-description-content">
+							Email: {this.props.email}
+						</p>
+						<p className="student-description-content">
+							Company: {this.props.company}
+						</p>
+						<p className="student-description-content">
+							Skill: {this.props.skill}
+						</p>
+						<p className="student-description-content">
+							Average: {this.calculateAverage(this.props.grades)}
+						</p>
+					</div>
+				</div>
+				<div className="student-toggle" onClick={() => this.handleToggle()}>
+					{this.renderToggle()}
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
-function calculateAverage(gradeArr) {
-	return (
-		gradeArr.reduce((a, b) => {
-			return Number(a) + Number(b);
-		}) / gradeArr.length
-	).toFixed(2);
-}
+export default StudentCard;
